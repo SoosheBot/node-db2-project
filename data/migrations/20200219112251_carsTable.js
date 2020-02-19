04e6a3a5d1ac7d0a31a2de43b19daae2148ce10f
sooshe <sooshe@sooshebot.com>
2020-02-19 11:24:06 -0600

    setup carsTable and salesTable migrations

--- data/migrations/20200219112251_carsTable.js
+++ data/migrations/20200219112251_carsTable.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('cars', tbl => {
+        tbl.increments('id');
+        tbl.string('VIN', 17).unique().notNullable();
+        tbl.string('make', 50).notNullable();
+        tbl.string('model', 255).notNullable();
+        tbl.integer('mileage').notNullable();
+        tbl.string('transmission_type');
+        tbl.string('title_status');
   
-};
-
-exports.down = function(knex) {
+    })
+  };
   
-};
+  exports.down = function(knex) {
+      return knex.schema.dropTableIfExists('cars');
+    };
+  
--- data/migrations/20200219112308_salesTable.js
+++ data/migrations/20200219112308_salesTable.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex) {
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("sales", tbl => {
+      tbl.increments("sales_id");
+      tbl.boolean("sold").defaultTo(false);
+    });
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("sales");
+  };
   
-};
