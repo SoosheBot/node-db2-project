7957e280fa65642db496e1b2d85e23000020c7cd
sooshe <sooshe@sooshebot.com>
2020-02-18 20:35:17 -0600

    setup dotenv

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+require('dotenv').config(); 
+
 const server = require('./server');
 
-const port = process.env.PORT || 7000;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+const PORT = process.env.PORT || "7000";
+
+server.listen(PORT, () => console.log(`\n** running on port: ${PORT} **\n`));
